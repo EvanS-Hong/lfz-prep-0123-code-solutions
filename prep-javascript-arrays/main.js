6923c7cb9964da0af7a820763a6f65090f0b6b4c
Evan Hong <e.hong1424@gmail.com>
2023-01-18 03:31:48 +0000

    prep-javascript-arrays

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -10,5 +10,5 @@ console.log('The 3rd element of the array is', thirdElement);
 var length = languages.length;
 console.log('The length of the array is', length);
 
-var lastElement = languages[6 - 1];
+var lastElement = languages[length - 1];
 console.log('The last element of the array is', lastElement);
